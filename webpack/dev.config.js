177e253f894d74e02fa0dd24d23d54cbab6f9b65
Kiryl Stasevich <kirylstasevich@gmail.com>
2018-01-04 14:49:51 +0300

    changed devtool for development

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -2,6 +2,6 @@ const merge = require('webpack-merge');
 const base = require('./base.config');
 
 module.exports = merge(base, {
-  devtool: 'source-maps',
+  devtool: 'cheap-module-eval-source-map',
   watch: true,
 });
