277118adb685a915104951e775282401f8327b26
denis.zhidis <denis_zhidis@mail.ru>
2018-02-05 17:54:57 +0300

    added test endpoint for employee

--- server/API/controllers/SomeController.ts
+++ server/API/controllers/SomeController.ts
@@ -14,7 +14,7 @@ import { injectable, inject } from 'inversify';
 
 import { ISomeService } from './../../Domain/Services/index';
 
-@controller('/api/some')
+@controller('/api')
 export class SomeController implements interfaces.Controller {
   private readonly _someService: ISomeService;
 
@@ -23,11 +23,44 @@ export class SomeController implements interfaces.Controller {
   }
 
   @httpGet('/')
-  private index(
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-  ): string {
+  private index(req: express.Request, res: express.Response): string {
     return this._someService.getString();
+  }
+
+  @httpGet('/employee')
+  private getEmployee(req: express.Request, res: express.Response): void {
+    res.json([
+      {
+        id: 1,
+        photoUrl: 'https://vk.com/images/camera_200.png',
+        fullName: 'Vasya Pupkin',
+        archived: false,
+      },
+      {
+        id: 2,
+        photoUrl: 'https://vk.com/images/camera_200.png',
+        fullName: 'Petya Pupkin',
+        archived: false,
+      },
+      {
+        id: 3,
+        photoUrl: 'https://vk.com/images/camera_200.png',
+        fullName: 'Dima Petrov',
+        archived: false,
+      },
+      {
+        id: 4,
+        photoUrl:
+          'https://pp.userapi.com/c836738/v836738191/6de55/3wEYIHussZI.jpg',
+        fullName: 'Alexandra Tsvirko',
+        archived: false,
+      },
+      {
+        id: 5,
+        photoUrl: 'https://vk.com/images/camera_200.png',
+        fullName: 'Ivan Ivanov',
+        archived: false,
+      },
+    ]);
   }
 }
