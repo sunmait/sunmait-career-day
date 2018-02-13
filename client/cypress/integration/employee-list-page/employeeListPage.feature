999ca58722e9fc0233d7cae8759c1aeee2f39127
Kiryl Stasevich <kirylstasevich@gmail.com>
2018-02-13 17:08:15 +0300

    added tests for employees page

--- client/cypress/integration/employee-list-page/employeeListPage.feature
+++ client/cypress/integration/employee-list-page/employeeListPage.feature
@@ -1,9 +1,19 @@
 Feature: Employee list page
 
   Scenario: I see 'Employee list' page header
+    Given I logged in as 'unit manager'
+    When I see 'employees' page
+    Then I see 'List Of Employees' page header
 
   Scenario: I see employee list
+    Given I logged in as 'unit manager'
+    When I see 'employees' page
+    Then I see list
 
   Scenario: I see employee data in list of employees
 
   Scenario: I am redirected to career day page by click on employee
+    Given I logged in as 'unit manager'
+    And I see 'employees' page
+    When I click on 'Alexandra Tsvirko' link
+    Then I am redirected to 'employees/4' page
--- client/cypress/support/step_definitions/common.js
+++ client/cypress/support/step_definitions/common.js
@@ -1,10 +1,24 @@
-given('I open {string} page', (page) => {
+let openPage = (page) => {
   cy.visit(`http://localhost:3000/${page}`);
-});
+};
+
+let checkPage = (page) => {
+  cy.url(`http://localhost:3000/${page}`);
+}
+
+given('I open {string} page', openPage);
+
+given('I see {string} page', (checkPage));
 
 given('I see {string} button', (buttonText) => {
   cy.get('button')
     .contains(buttonText);
+});
+
+given('I logged in as {string}', (userRole) => {
+  cy.visit(`http://localhost:3000/login`)
+  .get('button').contains(`Login as ${userRole}`)
+  .click();
 });
 
 when('I click on {string} button', (buttonText) => {
@@ -13,6 +27,22 @@ when('I click on {string} button', (buttonText) => {
     .click();
 });
 
-then('I am redirected to {string} page', (page) => {
-  cy.url(`http://localhost:3000/${page}`);
+when('I click on {string} link', (linkText) => {
+  cy.get('a')
+    .contains(linkText)
+    .click();
 });
+
+when('I open {string} page', openPage);
+
+when('I see {string} page', (checkPage));
+
+then('I am redirected to {string} page', (checkPage));
+
+then('I see {string} page header', (pageHeader) => {
+  cy.get('h1').contains(pageHeader);
+})
+
+then('I see list', () => {
+  cy.get('li');
+})
