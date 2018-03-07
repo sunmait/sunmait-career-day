1f828cdfe6aaac63fffb4dbaa2a90641ebe2d893
Aliaksandra Tsvirko <tsvirko1997@mail.ru>
2018-03-07 19:30:49 +0300

    finished writing tests

--- client/tests/src/redux/modules/employees/employeesActions.test.js
+++ client/tests/src/redux/modules/employees/employeesActions.test.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 describe('employees action', () => {
   beforeEach(() => {
     axios.get.mockReturnValue(Promise.resolve({status: 200, body: {}}));
+    axios.post.mockReturnValue(Promise.resolve({status: 200, body: {}}));
+    axios.delete.mockReturnValue(Promise.resolve({status: 200, body: {}}));
+    axios.patch.mockReturnValue(Promise.resolve({status: 200, body: {}}));
   });
 
   describe('method getEmployeesList', () => {
@@ -37,8 +40,6 @@ describe('employees action', () => {
         status: 200,
       };
 
-      axios.get.mockReturnValue(Promise.resolve(fakeResponse));
-
       return actions.getEmployeesList()(dispatchSpy)
         .then(() => expect(axios.get()).resolves.toEqual(fakeResponse));
     });
@@ -66,17 +67,15 @@ describe('employees action', () => {
       PhotoUrl: 'my-avatar',
       AccessToken: 'token',
     };
-    const fakeResponse = {
-      body: {},
-      status: 500,
-    };
-    test('should sent GET request to `/api/career-days/1`', () => {
+
+    test('should sent GET request to `/api/career-days/id`', () => {
       const dispatchSpy = jest.fn();
+
       return actions.getCareerDayOfEmployee(employee)(dispatchSpy)
         .then(() => {
           const expectedUrl = axios.get.mock.calls[0][0];
 
-          return expect(expectedUrl).toBe('/api/career-days/1');
+          return expect(expectedUrl).toBe(`/api/career-days/${employee.id}`);
         });
     });
 
@@ -92,58 +91,13 @@ describe('employees action', () => {
     });
 
     test('should dispatch correct payload in case of success response', () => {
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
       const dispatchSpy = jest.fn();
-
-      axios.get.mockReturnValue(Promise.resolve(fakeResponse));
 
       return actions.getCareerDayOfEmployee(employee)(dispatchSpy)
-        .then(() => expect(axios.get()).resolves.toEqual(fakeResponse));
-    });
-
-    test('should return rejected promise in case of error in request', () => {
-      const dispatchSpy = jest.fn();
-
-      axios.get.mockReturnValue(Promise.reject(fakeResponse));
-
-      return actions.getCareerDayOfEmployee(employee)(dispatchSpy)
-        .then(() => expect(axios.get()).rejects.toEqual(fakeResponse));
-    });
-  });
-
-  describe('method getSelectedCareerDay',() => {
-    const fakeResponse = {
-      body: {},
-      status: 500,
-    };
-    test('should sent GET request to `/api/objectives/1`',() => {
-      const dispatchSpy = jest.fn();
-      const numberOfEmployee = 1;
-
-      return actions.getSelectedCareerDay(numberOfEmployee)(dispatchSpy)
-        .then(() => {
-          const expectedUrl = axios.get.mock.calls[0][0];
-
-          return expect(expectedUrl).toBe('/api/objectives/1');
-        });
-    });
-
-    test('should dispatch correct action in case of success response',() => {
-      const dispatchSpy = jest.fn();
-
-      return actions.getSelectedCareerDay()(dispatchSpy)
-        .then(() => {
-          const type = dispatchSpy.mock.calls[0][0].type;
-
-          return expect(type).toEqual(EMPLOYEES_LIST.GET_SELECTED_CAREER_DAY)
-        });
-    });
-
-    test('should dispatch correct payload in case of success response', () => {
-      const dispatchSpy = jest.fn();
-
-      axios.get.mockReturnValue(Promise.resolve(fakeResponse));
-
-      return actions.getSelectedCareerDay()(dispatchSpy)
         .then(() => expect(axios.get()).resolves.toEqual(fakeResponse));
     });
 
@@ -153,15 +107,65 @@ describe('employees action', () => {
         body: {},
         status: 500,
       };
-    
+
       axios.get.mockReturnValue(Promise.reject(fakeResponse));
 
-      return actions.getSelectedCareerDay()(dispatchSpy)
+      return actions.getCareerDayOfEmployee(employee)(dispatchSpy)
         .then(() => expect(axios.get()).rejects.toEqual(fakeResponse));
     });
   });
 
-  describe('method getSelectedEmployee',() => {
+  describe('method getSelectedCareerDay', () => {
+    const careerDayId = 1;
+
+    test('should sent GET request to `/api/objectives/careerDayId`', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.getSelectedCareerDay(careerDayId)(dispatchSpy)
+        .then(() => {
+          const expectedUrl = axios.get.mock.calls[0][0];
+
+          return expect(expectedUrl).toBe(`/api/objectives/${careerDayId}`);
+        });
+    });
+
+    test('should dispatch correct action in case of success response', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.getSelectedCareerDay(careerDayId)(dispatchSpy)
+        .then(() => {
+          const type = dispatchSpy.mock.calls[0][0].type;
+
+          return expect(type).toEqual(EMPLOYEES_LIST.GET_SELECTED_CAREER_DAY)
+        });
+    });
+
+    test('should dispatch correct payload in case of success response', () => {
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
+      const dispatchSpy = jest.fn();
+
+      return actions.getSelectedCareerDay(careerDayId)(dispatchSpy)
+        .then(() => expect(axios.get()).resolves.toEqual(fakeResponse));
+    });
+
+    test('should return rejected promise in case of error in request', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 500,
+      };
+
+      axios.get.mockReturnValue(Promise.reject(fakeResponse));
+
+      return actions.getSelectedCareerDay(careerDayId)(dispatchSpy)
+        .then(() => expect(axios.get()).rejects.toEqual(fakeResponse));
+    });
+  });
+
+  describe('method getSelectedEmployee', () => {
     const employee = {
       id: 1,
       Roles: '1',
@@ -170,11 +174,11 @@ describe('employees action', () => {
       PhotoUrl: 'my-avatar',
       AccessToken: 'token',
     };
-    
-    test('should dispatch correct action',() => {
+
+    test('should dispatch correct action', () => {
       const dispatchSpy = jest.fn();
 
-      actions.getSelectedEmployee()(dispatchSpy);
+      actions.getSelectedEmployee(employee)(dispatchSpy);
       const type = dispatchSpy.mock.calls[0][0].type;
 
       expect(type).toEqual(EMPLOYEES_LIST.GET_SELECTED_EMPLOYEE);
@@ -182,25 +186,25 @@ describe('employees action', () => {
 
     test('should dispatch correct payload', () => {
       const dispatchSpy = jest.fn();
-      
+
       actions.getSelectedEmployee(employee)(dispatchSpy);
       const returnedData = dispatchSpy.mock.calls[0][0].payload;
 
       expect(returnedData).toEqual(employee);
     });
-
   });
 
-  describe('method addCareerDay',() => {
-    const fakeResponse = {
-      body: {},
-      status: 200,
+  describe('method addCareerDay', () => {
+    const careerDay = {
+      EmployeeExternalId: 1,
+      UnitManagerExternalId: 1,
+      InterviewDate: new Date(),
     };
-    
-    test('should sent POST request to `/api/career-days`',() => {
+
+    test('should sent POST request to `/api/career-days`', () => {
       const dispatchSpy = jest.fn();
-      
-      return actions.addCareerDay()(dispatchSpy)
+
+      return actions.addCareerDay(careerDay)(dispatchSpy)
         .then(() => {
           const expectedUrl = axios.post.mock.calls[0][0];
 
@@ -208,10 +212,10 @@ describe('employees action', () => {
         });
     });
 
-    test('should dispatch correct action in case of success response',() => {
+    test('should dispatch correct action in case of success response', () => {
       const dispatchSpy = jest.fn();
 
-      return actions.addCareerDay()(dispatchSpy)
+      return actions.addCareerDay(careerDay)(dispatchSpy)
         .then(() => {
           const type = dispatchSpy.mock.calls[0][0].type;
 
@@ -220,9 +224,13 @@ describe('employees action', () => {
     });
 
     test('should dispatch correct payload in case of success response', () => {
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
       const dispatchSpy = jest.fn();
-      
-      return actions.addCareerDay()(dispatchSpy)
+
+      return actions.addCareerDay(careerDay)(dispatchSpy)
         .then(() => expect(axios.post()).resolves.toEqual(fakeResponse));
     });
 
@@ -235,7 +243,7 @@ describe('employees action', () => {
 
       axios.post.mockReturnValue(Promise.reject(fakeResponse));
 
-      return actions.addCareerDay()(dispatchSpy)
+      return actions.addCareerDay(careerDay)(dispatchSpy)
         .then(() => expect(axios.post()).rejects.toEqual(fakeResponse));
     });
   });
@@ -246,6 +254,7 @@ describe('employees action', () => {
       Description: 'description',
       CareerDayId: 1,
     };
+
     test('should sent POST request to `/api/objectives`', () => {
       const dispatchSpy = jest.fn();
 
@@ -259,8 +268,10 @@ describe('employees action', () => {
 
     test('should dispatch correct action in case of success response', () => {
       const dispatchSpy = jest.fn();
-      
-      axios.post.mockReturnValue(Promise.resolve({status: 200, body: {}}));
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
 
       return actions.addObjective(objective)(dispatchSpy)
         .then(() => {
@@ -277,8 +288,6 @@ describe('employees action', () => {
         status: 200,
       };
 
-      axios.post.mockReturnValue(Promise.resolve(fakeResponse));
-
       return actions.addObjective(objective)(dispatchSpy)
         .then(() => expect(axios.post()).resolves.toEqual(fakeResponse));
     });
@@ -295,5 +304,209 @@ describe('employees action', () => {
       return actions.addObjective(objective)(dispatchSpy)
         .then(() => expect(axios.post()).rejects.toEqual(fakeResponse));
     });
-  });  
+  });
+
+  describe('method deleteCareerDay', () => {
+    const careerDayId = 1;
+
+    test('should sent DELETE request to `/api/career-days/careerDayId`', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.deleteCareerDay(careerDayId)(dispatchSpy)
+        .then(() => {
+          const expectedUrl = axios.delete.mock.calls[0][0];
+
+          return expect(expectedUrl).toBe(`/api/career-days/${careerDayId}`);
+        });
+    });
+
+    test('should dispatch correct action in case of success response', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.deleteCareerDay(careerDayId)(dispatchSpy)
+        .then(() => {
+          const type = dispatchSpy.mock.calls[0][0].type;
+
+          return expect(type).toEqual(EMPLOYEES_LIST.DELETE_CAREER_DAY);
+        });
+    });
+
+    test('should dispatch correct payload in case of success response', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
+
+      return actions.deleteCareerDay(careerDayId)(dispatchSpy)
+        .then(() => expect(axios.delete()).resolves.toEqual(fakeResponse));
+    });
+
+    test('should return rejected promise in case of error in request', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 500,
+      };
+
+      axios.delete.mockReturnValue(Promise.reject(fakeResponse));
+
+      return actions.deleteCareerDay(careerDayId)(dispatchSpy)
+        .then(() => expect(axios.delete()).rejects.toEqual(fakeResponse));
+    });
+  });
+
+  describe('method updateObjective', () => {
+    const objective = {
+      id: 1,
+      title: 'title',
+      description: 'description',
+    };
+
+    test('should sent PATCH request to `/api/objectives/id`', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.updateObjective(objective)(dispatchSpy)
+        .then(() => {
+          const expectedUrl = axios.patch.mock.calls[0][0];
+
+          return expect(expectedUrl).toBe(`/api/objectives/${objective.id}`);
+        });
+    });
+
+    test('should dispatch correct action in case of success response', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.updateObjective(objective)(dispatchSpy)
+        .then(() => {
+          const type = dispatchSpy.mock.calls[0][0].type;
+
+          return expect(type).toEqual(EMPLOYEES_LIST.UPDATE_OBJECTIVE);
+        });
+    });
+
+    test('should dispatch correct payload in case of success response', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
+
+      return actions.updateObjective(objective)(dispatchSpy)
+        .then(() => expect(axios.patch()).resolves.toEqual(fakeResponse));
+    });
+
+    test('should return rejected promise in case of error in request', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 500,
+      };
+
+      axios.patch.mockReturnValue(Promise.reject(fakeResponse));
+
+      return actions.updateObjective(objective)(dispatchSpy)
+        .then(() => expect(axios.patch()).rejects.toEqual(fakeResponse));
+    });
+  });
+
+  describe('method archiveCareerDay', () => {
+    const careerDayId = 1;
+
+    test('should sent PATCH request to `/api/career-days/archive/careerDayId`', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.archiveCareerDay(careerDayId)(dispatchSpy)
+        .then(() => {
+          const expectedUrl = axios.patch.mock.calls[0][0];
+
+          return expect(expectedUrl).toBe(`/api/career-days/archive/${careerDayId}`);
+        });
+    });
+
+    test('should dispatch correct action in case of success response', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.archiveCareerDay(careerDayId)(dispatchSpy)
+        .then(() => {
+          const type = dispatchSpy.mock.calls[0][0].type;
+
+          return expect(type).toEqual(EMPLOYEES_LIST.ARCHIVE_CAREER_DAY);
+        });
+    });
+
+    test('should dispatch correct payload in case of success response', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
+
+      return actions.archiveCareerDay(careerDayId)(dispatchSpy)
+        .then(() => expect(axios.patch()).resolves.toEqual(fakeResponse));
+    });
+
+    test('should return rejected promise in case of error in request', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 500,
+      };
+
+      axios.patch.mockReturnValue(Promise.reject(fakeResponse));
+
+      return actions.archiveCareerDay(careerDayId)(dispatchSpy)
+        .then(() => expect(axios.patch()).rejects.toEqual(fakeResponse));
+    });
+  });
+
+  describe('method deleteObjective', () => {
+    const objectiveId = 1;
+
+    test('should sent DELETE request to `/api/objectives/objectiveId`', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.deleteObjective(objectiveId)(dispatchSpy)
+        .then(() => {
+          const expectedUrl = axios.delete.mock.calls[0][0];
+
+          return expect(expectedUrl).toBe(`/api/objectives/${objectiveId}`);
+        });
+    });
+
+    test('should dispatch correct action in case of success response', () => {
+      const dispatchSpy = jest.fn();
+
+      return actions.deleteObjective(objectiveId)(dispatchSpy)
+        .then(() => {
+          const type = dispatchSpy.mock.calls[0][0].type;
+
+          return expect(type).toEqual(EMPLOYEES_LIST.DELETE_OBJECTIVE);
+        });
+    });
+
+    test('should dispatch correct payload in case of success response', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 200,
+      };
+
+      return actions.deleteObjective(objectiveId)(dispatchSpy)
+        .then(() => expect(axios.delete()).resolves.toEqual(fakeResponse));
+    });
+
+    test('should return rejected promise in case of error in request', () => {
+      const dispatchSpy = jest.fn();
+      const fakeResponse = {
+        body: {},
+        status: 500,
+      };
+
+      axios.delete.mockReturnValue(Promise.reject(fakeResponse));
+
+      return actions.deleteObjective(objectiveId)(dispatchSpy)
+        .then(() => expect(axios.delete()).rejects.toEqual(fakeResponse));
+    });
+  });
 });
