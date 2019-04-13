7859c11c7241acc7ca6e44f8406097691417e378
Denis Papakul <denis.papakul.dp@gmail.com>
2019-04-13 17:27:56 +0300

    Deleted whitespace

--- server/API/controllers/ObjectiveController.ts
+++ server/API/controllers/ObjectiveController.ts
@@ -25,7 +25,6 @@ import { Principal } from './../providers';
 export class ObjectiveController extends BaseHttpController {
   @inject('ObjectiveService')
   private readonly _objectiveService!: IObjectiveService;
-  
   @inject('CareerDayService')
   private readonly _careerDayService!: ICareerDayService;
 
@@ -97,7 +96,6 @@ export class ObjectiveController extends BaseHttpController {
     @response() res: express.Response,
   ): Promise<void> {
     const user = this.httpContext.user as Principal;
-    
     res.json(
       await this._objectiveService.updateObjectiveEmployee(
         id,
